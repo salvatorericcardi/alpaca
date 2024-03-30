a2c5afc6a15eb614a281f0c1ada44115e5303caf
riccardi1997 <riccardi.salvatore.97@gmail.com>
2024-03-30 17:40:33 +0100

    fix html styles with media queries and add some script for the future

--- script.js
+++ script.js
@@ -12,6 +12,15 @@ const resetRadioButtons = inputs => {
     }
 }
 
+/* const isScrolledToRight = (el) => {
+    // IT WORKS
+    // NOTE: scrollLeft is fractional, while scrollWidth and clientWidth are
+    // not, so without this Math.abs() trick then sometimes the result won't
+    // work because scrollLeft may not be exactly equal to el.scrollWidth -
+    // el.clientWidth when scrolled to the right
+    return Math.abs(el.scrollWidth - el.clientWidth - el.scrollLeft) * 0.66 < 1
+} */
+
 const lastAlpaca = {
     accessories: null,
     backgrounds: null,
@@ -45,7 +54,7 @@ for(const el of document.getElementsByClassName("accessory")) {
         // if label is selected set input checked if not remove checked
         if(isSelected(e.target)) {
             e.target.previousElementSibling.setAttribute("checked", true)
-            document.getElementById(`${e.target.previousElementSibling.value}-style`).style.display = "block"
+            document.getElementById(`${e.target.previousElementSibling.value}-style`).style.display = "contents"
             
             selectedEl[e.target.previousElementSibling.value] = e.target.previousElementSibling.value
         } else {
@@ -97,6 +106,47 @@ for(const el of document.getElementsByClassName("styles")) {
     })
 }
 
+// every arrow button has on click event
+for(const el of document.getElementsByClassName("arrow")) {
+    el.addEventListener("click", (e) => {
+        e.preventDefault()
+        const parent = e.target.parentElement
+        // const arrows = document.getElementsByClassName("arrow")
+
+        // if click on right arrow and not scrollend enter in the loop
+        if(e.target.classList.contains("right") /* && !isScrolledToRight(parent.previousElementSibling) */) {
+            // move to the right
+            parent.previousElementSibling.scrollLeft += parent.previousElementSibling.getBoundingClientRect().width
+
+            /* if(arrows[0].classList.contains("disabled")) {
+                arrows[0].classList.remove("disabled")
+                arrows[0].disabled = false
+            }
+
+            // if container position at end disable right arrow
+            if(isScrolledToRight(parent.previousElementSibling)) {
+                arrows[1].classList.add("disabled")
+                arrows[1].disabled = true
+            } */
+        // if click on left arrow and not scrollstart enter in the loop
+        } else if (e.target.classList.contains("left") /* && Math.abs(parent.nextElementSibling.scrollLeft) >= 1 */) {
+            // move to the left
+            parent.nextElementSibling.scrollLeft -= parent.nextElementSibling.getBoundingClientRect().width
+
+            /* if(arrows[1].classList.contains("disabled")) {
+                arrows[1].classList.remove("disabled")
+                arrows[1].disabled = false
+            }
+
+            // if container position at start disable left arrow 
+            if(!arrows[0].classList.contains("disabled") && Math.abs(parent.nextElementSibling.scrollLeft) < 1) {              
+                arrows[0].classList.add("disabled")
+                arrows[0].disabled = true
+            } */
+        }
+    })
+}
+
 // random alpaca button
 document.getElementById("random").addEventListener("click", () => {
     for(const key of Object.keys(alpaca)) {
@@ -132,5 +182,5 @@ document.getElementById("download").addEventListener('click', () => {
 })
 
 document.addEventListener("DOMContentLoaded", () => {
-    confirm("Do you want to clear localStorage") ? localStorage.clear() : null
+    confirm("Do you want to clear localStorage?") ? localStorage.clear() : null
 })
